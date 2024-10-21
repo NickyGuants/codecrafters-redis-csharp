ab18e4b88eefc55bfb9c2563a18541105c232e1b
Nicholas Guantai <nicholasguantai528@gmail.com>
2024-10-21 11:51:59 +0300

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -7,4 +7,14 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+
+using Socket client = server.AcceptSocket(); // wait for client
+
+if (client.Connected) { 
+    Console.WriteLine("Connected");
+
+    //Send back a response
+    var response = "+PONG\r\n";
+    byte[] msg = System.Text.Encoding.ASCII.GetBytes(response);
+    await client.SendAsync(msg);
+}
